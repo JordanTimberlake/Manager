b0c9397a6c52be3735e49de74d5e148abc440ee1
Jordan Timberlake <u21565882@tuks.co.za>
2024-08-05 00:43:54 +0200

    Updating CORS in DJANGO

--- Backend/api/api/settings.py
+++ Backend/api/api/settings.py
@@ -147,6 +147,18 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "https://manager-nsdv-fnbnbubj1-jordantimberlakes-projects.vercel.app",  # Update this with your Nuxt frontend URL
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 CORS_ALLOW_METHODS = [
     'GET',
