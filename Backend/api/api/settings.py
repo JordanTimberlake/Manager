d7f5203b501afcf3bbec4a20da30402a973837c7
Jordan Timberlake <u21565882@tuks.co.za>
2024-08-05 01:11:25 +0200

    More Updates

--- Backend/api/api/settings.py
+++ Backend/api/api/settings.py
@@ -145,21 +145,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # add this
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "https://manager-nsdv-fnbnbubj1-jordantimberlakes-projects.vercel.app",  # Update this with your Nuxt frontend URL
     'https://manager-nsdv.vercel.app/'
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
     'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
     'x-csrftoken',
-    'x-requested-with',
 ]
 
 CORS_ALLOW_METHODS = [
--- Manager/pages/signup.vue
+++ Manager/pages/signup.vue
@@ -72,6 +72,7 @@ const getCsrfToken = async () => {
     try {
         const data = await $fetch('https://vitreous-bert-jordantimberlake-dd542edd.koyeb.app/api/token/', {
             method: 'GET',
+            credentials: 'include', // Ensure cookies are included in the request
         });
         csrfToken.value = data.csrfToken;
     } catch (err) {
